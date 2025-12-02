3b81dd08f56c462adf9127f7cd3b94b6503621f6
Ilja Schwarz <iljaschwarz@gmail.com>
2025-12-02 16:34:57 +0100

    Refactor Day 01 logic

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,54 +1,54 @@
-
 import kotlin.math.abs
 
 
 fun main() {
 
     val input = readInput("Day01")
+
+    fun mod100(num: Int): Int {
+        return (num % 100 + 100) % 100
+    }
+
+    fun part1(input: List<String>) {
+        var pos = 50
+        var count = 0
+        for (line in input) {
+            val amount = if (line.startsWith("L", false)) {
+                line.substring(1).toInt() * -1
+            } else {
+                line.substring(1).toInt()
+            }
+
+            pos = mod100(pos + amount)
+
+            if (pos == 0) {
+                count++
+            }
+        }
+
+        println(count)
+    }
+
+    fun part2(input: List<String>) {
+        var pos = 50
+        var count = 0
+
+        for (line in input) {
+            val amount = if (line.startsWith("L", false)) {
+                line.substring(1).toInt() * -1
+            } else {
+                line.substring(1).toInt()
+            }
+            count += abs((pos + amount) / 100)
+            if (amount < 0 && pos != 0 && pos + amount <= 0)
+                count++
+
+            pos = mod100(pos + amount)
+
+        }
+        println(count)
+    }
+
     part1(input)
     part2(input)
-}
-
-fun part1(input: List<String>) {
-    var pos = 50
-    var count = 0
-    for (line in input) {
-        val amount = if (line.startsWith("L", false)) {
-            line.substring(1).toInt() * -1
-        } else {
-            line.substring(1).toInt()
-        }
-
-        pos = mod100(pos + amount)
-
-        if (pos == 0) {
-            count++
-        }
-    }
-
-    println(count)
-}
-
-fun part2(input: List<String>) {
-    var pos = 50
-    var count = 0
-
-    for (line in input) {
-        val amount = if (line.startsWith("L", false)) {
-            line.substring(1).toInt() * -1
-        } else {
-            line.substring(1).toInt()
-        }
-        count += abs((pos + amount) / 100)
-        if (amount < 0 && pos != 0 && pos + amount <= 0)
-            count++
-
-        pos = mod100(pos + amount)
-
-    }
-    println(count)
-}
-
-fun mod100(num: Int): Int {
-    return (num % 100 + 100) % 100
 }
